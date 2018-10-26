d788c80d8ee6e05b5fa677242446f0c5396ec72e
valoupeca <lamurev@gmail.com>
2018-10-25 21:24:21 -0400

    Ajout des différents écrans 2.0

--- app/src/main/java/com/example/lamur/exchangesstudents/Sign_in.java
+++ app/src/main/java/com/example/lamur/exchangesstudents/Sign_in.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class Sign_in extends AppCompatActivity {
 
+    DBHelper dbhelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        dbhelper = (DBHelper)this.getIntent().getSerializableExtra("DbHelper");
+
     }
 }
