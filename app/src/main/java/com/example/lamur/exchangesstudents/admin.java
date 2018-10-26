4b5e1b93e69b9f6e2751963cc1e905a44e4c3afd
MurruniRemi <remimurruni@hotmail.fr>
2018-10-25 20:31:50 -0400

    correction de fournisseur

--- app/src/main/java/com/example/lamur/exchangesstudents/admin.java
+++ app/src/main/java/com/example/lamur/exchangesstudents/admin.java
@@ -5,8 +5,4 @@ import java.util.ArrayList;
 
 public final class admin extends User{
 
-public void createService(String nom, float taux_horraire, ArrayList<services> categorie){
-    services _services = new services(nom,taux_horraire,categorie);
-}
-
 }
