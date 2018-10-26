1c106200f380ad1fc6c86ccec2c871ea3bc944f3
MurruniRemi <remimurruni@hotmail.fr>
2018-10-25 21:52:32 -0400

    correction de fournisseur

--- app/src/main/java/com/example/lamur/exchangesstudents/Proprietaire.java
+++ app/src/main/java/com/example/lamur/exchangesstudents/Proprietaire.java
@@ -1,4 +1,13 @@
 package com.example.lamur.exchangesstudents;
 
 public class Proprietaire extends User{
+
+
+    public Proprietaire(int _id, String _username, String mdp) {
+        super(_id, _username, mdp);
+    }
+
+    public Proprietaire(String _username, String mdp) {
+        super(_username, mdp);
+    }
 }
--- app/src/main/java/com/example/lamur/exchangesstudents/Sign_up.java
+++ app/src/main/java/com/example/lamur/exchangesstudents/Sign_up.java
@@ -2,16 +2,37 @@ package com.example.lamur.exchangesstudents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class Sign_up extends AppCompatActivity {
+
+    DBHelper dbhelper;
+    ListView role;
+    EditText username;
+    EditText password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        dbhelper = (DBHelper)this.getIntent().getSerializableExtra("DbHelper");
 
         ListView listview = (ListView) findViewById(R.id.role);
         String[] values = new String[] { "Fournisseur","Propriétaire" };
+
     }
+
+     protected void submit(){
+         username = (EditText) findViewById(R.id.username);
+         password = (EditText) findViewById(R.id.mdp);
+         role = (ListView) findViewById(R.id.role);
+
+
+        /*  "role.setList"*/
+         Proprietaire user = new Proprietaire(username.getText().toString(),password.getText().toString());
+     }
+
+
 }
