011f813cabb65d16f43c4479502b810f6f63a416
valoupeca <lamurev@gmail.com>
2018-10-25 21:40:02 -0400

    Ajout user écran

--- app/src/main/java/com/example/lamur/exchangesstudents/Sign_up.java
+++ app/src/main/java/com/example/lamur/exchangesstudents/Sign_up.java
@@ -2,6 +2,7 @@ package com.example.lamur.exchangesstudents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class Sign_up extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ListView listview = (ListView) findViewById(R.id.role);
+        String[] values = new String[] { "Fournisseur","Propriétaire" };
     }
 }
